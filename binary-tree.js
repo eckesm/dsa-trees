af71f8fcfc3a7d28fc69f6aafb3a42b041cd55c8
Matthew Eckes <eckesm@gmail.com>
2021-09-23 20:17:32 -0400

    Completed assignemnt.  Consulted solutions for maxDepth() and maxSum().

--- binary-tree.js
+++ binary-tree.js
@@ -1,75 +1,110 @@
 /** BinaryTreeNode: node for a general tree. */
 
 class BinaryTreeNode {
-  constructor(val, left = null, right = null) {
-    this.val = val;
-    this.left = left;
-    this.right = right;
-  }
+	constructor(val, left = null, right = null) {
+		this.val = val;
+		this.left = left;
+		this.right = right;
+	}
 }
 
 class BinaryTree {
-  constructor(root = null) {
-    this.root = root;
-  }
+	constructor(root = null) {
+		this.root = root;
+	}
 
-  /** minDepth(): return the minimum depth of the tree -- that is,
+	/** minDepth(): return the minimum depth of the tree -- that is,
    * the length of the shortest path from the root to a leaf. */
 
-  minDepth() {
+	minDepth() {
+		if (!this.root) return 0;
+		let depth = 1;
+		const toVisitQueue = [ this.root ];
+		while (toVisitQueue.length) {
+			const current = toVisitQueue.shift();
+			if (current.left === null && current.right === null) {
+				return depth;
+			}
+			else {
+				depth += 1;
+				if (current.left) toVisitQueue.push(current.left);
+				if (current.right) toVisitQueue.push(current.right);
+			}
+		}
+	}
 
-  }
-
-  /** maxDepth(): return the maximum depth of the tree -- that is,
+	/** maxDepth(): return the maximum depth of the tree -- that is,
    * the length of the longest path from the root to a leaf. */
 
-  maxDepth() {
+	maxDepth() {
+		if (!this.root) return 0;
 
-  }
+		function maxDepthHelper(node) {
+			if (node.left === null && node.right === null) return 1;
+			if (node.left === null) return maxDepthHelper(node.right) + 1;
+			if (node.right === null) return maxDepthHelper(node.left) + 1;
+			return Math.max(maxDepthHelper(node.left), maxDepthHelper(node.right)) + 1;
+		}
 
-  /** maxSum(): return the maximum sum you can obtain by traveling along a path in the tree.
+		return maxDepthHelper(this.root);
+	}
+
+	/** maxSum(): return the maximum sum you can obtain by traveling along a path in the tree.
    * The path doesn't need to start at the root, but you can't visit a node more than once. */
 
-  maxSum() {
+	maxSum() {
+		let result = 0;
 
-  }
+		function maxSumHelper(node) {
+			if (node === null) return 0;
+			const leftSum = maxSumHelper(node.left);
+			const rightSum = maxSumHelper(node.right);
+			result = Math.max(result, node.val + leftSum + rightSum);
+			return Math.max(0, leftSum + node.val, rightSum + node.val);
+		}
 
-  /** nextLarger(lowerBound): return the smallest value in the tree
+		maxSumHelper(this.root);
+		return result;
+	}
+
+	/** nextLarger(lowerBound): return the smallest value in the tree
    * which is larger than lowerBound. Return null if no such value exists. */
 
-  nextLarger(lowerBound) {
+	nextLarger(lowerBound) {
+		if (!this.root) return null;
 
-  }
+		let smallest = null;
+		const toVisitStack = [ this.root ];
+		while (toVisitStack.length) {
+			const current = toVisitStack.pop();
+			if ((current.val < smallest || smallest === null) && current.val > lowerBound) smallest = current.val;
+			if (current.left) toVisitStack.push(current.left);
+			if (current.right) toVisitStack.push(current.right);
+		}
+		return smallest;
+	}
 
-  /** Further study!
+	/** Further study!
    * areCousins(node1, node2): determine whether two nodes are cousins
    * (i.e. are at the same level but have different parents. ) */
 
-  areCousins(node1, node2) {
+	areCousins(node1, node2) {}
 
-  }
-
-  /** Further study!
+	/** Further study!
    * serialize(tree): serialize the BinaryTree object tree into a string. */
 
-  static serialize() {
+	static serialize() {}
 
-  }
-
-  /** Further study!
+	/** Further study!
    * deserialize(stringTree): deserialize stringTree into a BinaryTree object. */
 
-  static deserialize() {
+	static deserialize() {}
 
-  }
-
-  /** Further study!
+	/** Further study!
    * lowestCommonAncestor(node1, node2): find the lowest common ancestor
    * of two nodes in a binary tree. */
 
-  lowestCommonAncestor(node1, node2) {
-    
-  }
+	lowestCommonAncestor(node1, node2) {}
 }
 
 module.exports = { BinaryTree, BinaryTreeNode };
--- tree.js
+++ tree.js
@@ -1,35 +1,69 @@
 /** TreeNode: node for a general tree. */
 
 class TreeNode {
-  constructor(val, children = []) {
-    this.val = val;
-    this.children = children;
-  }
+	constructor(val, children = []) {
+		this.val = val;
+		this.children = children;
+	}
 }
 
 class Tree {
-  constructor(root = null) {
-    this.root = root;
-  }
+	constructor(root = null) {
+		this.root = root;
+	}
 
-  /** sumValues(): add up all of the values in the tree. */
+	/** sumValues(): add up all of the values in the tree. */
 
-  sumValues() {
-    
-  }
+	sumValues() {
+		let sum = 0;
+		if (!this.root) return 0;
+		const toVisitStack = [ this.root ];
+		while (toVisitStack.length) {
+			const current = toVisitStack.pop();
+			sum += current.val;
+			for (let child of current.children) {
+				toVisitStack.push(child);
+			}
+		}
+		return sum;
+	}
 
-  /** countEvens(): count all of the nodes in the tree with even values. */
+	/** countEvens(): count all of the nodes in the tree with even values. */
 
-  countEvens() {
+	countEvens() {
+		let evens = 0;
+		if (!this.root) return 0;
+		const toVisitStack = [ this.root ];
+		while (toVisitStack.length) {
+			const current = toVisitStack.pop();
+			if (current.val % 2 === 0) {
+				evens += 1;
+			}
+			for (let child of current.children) {
+				toVisitStack.push(child);
+			}
+		}
+		return evens;
+	}
 
-  }
-
-  /** numGreater(lowerBound): return a count of the number of nodes
+	/** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
-  numGreater(lowerBound) {
-
-  }
+	numGreater(lowerBound) {
+		let greaterLB = 0;
+		if (!this.root) return 0;
+		const toVisitStack = [ this.root ];
+		while (toVisitStack.length) {
+			const current = toVisitStack.pop();
+			if (current.val > lowerBound) {
+				greaterLB += 1;
+			}
+			for (let child of current.children) {
+				toVisitStack.push(child);
+			}
+		}
+		return greaterLB;
+	}
 }
 
 module.exports = { Tree, TreeNode };
